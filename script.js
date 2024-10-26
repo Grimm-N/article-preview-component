4ff655221cc8592552e1b81a023fb51ae1a02ddb
Grimm-N <natali.grimm.mim@gmail.com>
2024-10-26 15:55:57 +0300

    update

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const shareButton = document.querySelector('.article__share-btn');
+document.addEventListener('DOMContentLoaded', function () {
+    const shareButton = document.querySelector('.article__info-author-btn-share');
+    const shareLinks = document.querySelector('.article__info-author-links');
   
-    // Переключаем видимость ссылок на социальные сети
-    shareButton.addEventListener('click', toggleShareLinks);
+    shareLinks.style.display = 'none';
+  
+    shareButton.addEventListener('click', function (event) {
+      event.stopPropagation();
+      toggleShareLinks();
+      shareButton.classList.toggle('active'); 
+    });
+  
+    document.addEventListener('click', function () {
+      if (shareLinks.classList.contains('show')) {
+        hideShareLinks();
+        shareButton.classList.remove('active');
+      }
+    });
   });
   
-  // Функция для переключения видимости ссылок
   function toggleShareLinks() {
-    const shareLinks = document.querySelector('.article__share-links');
-    // Проверяем текущее состояние и меняем его на противоположное
-    shareLinks.style.display = (shareLinks.style.display === 'none' || !shareLinks.style.display) 
-      ? 'flex' 
-      : 'none';
+    const shareLinks = document.querySelector('.article__info-author-links');
+    if (shareLinks.classList.contains('show')) {
+      hideShareLinks();
+    } else {
+      showShareLinks();
+    }
+  }
+  
+  function showShareLinks() {
+    const shareLinks = document.querySelector('.article__info-author-links');
+    shareLinks.style.display = 'flex';
+    requestAnimationFrame(() => {
+      shareLinks.classList.add('show');
+    });
+  }
+  
+  function hideShareLinks() {
+    const shareLinks = document.querySelector('.article__info-author-links');
+    shareLinks.classList.remove('show');
+    shareLinks.addEventListener('transitionend', function () {
+      shareLinks.style.display = 'none';
+    }, { once: true });
   }
   
